7272c704c5e499760692ac39e7c8ff539b8d66ac
Ryan Workman <rdavid1099@gmail.com>
2019-03-31 11:34:57 -0700

    Make basic text to launch buildable application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h2>Slack Slash Command /lmgtfy</h2>
         </header>
+        <p>
+          Wrapping <a href="http://lmgtfy.com/">LMGTFY</a> using a simple slack command.
+        </p>
       </div>
     );
   }
